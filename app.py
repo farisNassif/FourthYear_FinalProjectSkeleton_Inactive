619504d94692b6dbca6ac6537d7a6cb9d6f5ad86
farisNassif <g00347032@gmit.ie>
2019-09-27 19:34:48 +0100

    POST functionality implemented, saving to database and displaying back to user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -19,7 +19,23 @@ class Todo(db.Model):
 # Index route so when I browse to the url it doesn't 404
 @app.route('/', methods=['Post', 'GET'])
 def index():
-    return render_template('index.html')
+    # Whenever someone wants to add some input 
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        # Error Handling
+        except: 
+            return 'Issue adding input'
+    else:
+        # Looks at database contents in order they were created and show all of them
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        # Base Page
+        return render_template('index.html', tasks=tasks)
 
 if __name__ == "__main__":
     # If theres any errors they'll pop up on the page
