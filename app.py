e1f29e9364525b8f365fd925ce1054020f182d17
farisNassif <g00347032@gmit.ie>
2019-09-27 19:02:14 +0100

    sqllite setup with app temporarily, will use another db at later point

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+# Where the database is located
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Return a string everytime make a new element
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 # Index route so when I browse to the url it doesn't 404
-
 @app.route('/')
 def index():
     return render_template('index.html')
